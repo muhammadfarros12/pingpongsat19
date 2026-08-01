36bd1bee10c4aeabf6f85a689d3cced6fccb0143
Muhammad Farroos <70476078+muhammadfarros12@users.noreply.github.com>
2026-08-01 20:21:45 +0700

    Implement GameSprite class for game objects
    Added GameSprite class to handle player and ball sprites.

--- main.py
+++ main.py
@@ -9,6 +9,23 @@ background_color = (25, 184, 209)
 
 window.fill(background_color)
 
+class GameSprite(sprite.Sprite):
+   def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+       super().__init__()
+       self.image = transform.scale(image.load(player_image), (wight, height)) #e.g. 55,55 - parameters
+       self.speed = player_speed
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
+
+
+   def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
+
+player1 = GameSprite('racket.png', 30, 200, 4, 50, 150)
+player2 = GameSprite('racket.png', 520, 200, 4, 50, 150)
+ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
+
 game = True
 FPS = 60
 clock = time.Clock()
@@ -17,6 +34,10 @@ while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+    
+    player1.reset()
+    player2.reset()
+    ball.reset()
 
     
     display.update()
